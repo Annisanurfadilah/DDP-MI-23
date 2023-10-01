32e997a537c1a8d5f953a06e794439535e2124fd
Annisanurfadilah <nsaflhhh@gmail.com>
2023-10-01 12:28:59 +0700

    https://github.com/Annisanurfadilah/DDP-MI-23.git

---
+++ 03 Percabangan/if4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nilai;
+    cin>>nilai;
+    
+    if (nilai % 2 == 0) {
+        cout<<"Genap"<<endl;
+    } else {
+        cout<<"Ganjil"<<endl;
+}
+}
